5394e44904b55a0cb69476df0931811bda39d684
toptensor-deployer <deployer@toptensor.ai>
2026-05-09 05:23:41 +0000

    feat: add 900-vertex graph problem tier

--- CliqueAI/selection/problem_selector.py
+++ CliqueAI/selection/problem_selector.py
@@ -23,7 +23,7 @@ PROBLEMS = [
         label="general",
         vertex_range=Range(min=290, max=300),
         edge_range=Range(min=100, max=max_int),
-        difficulty=0.6,
+        difficulty=0.7,
     ),
     Problem(
         label="general",
@@ -35,15 +35,14 @@ PROBLEMS = [
         label="general",
         vertex_range=Range(min=690, max=700),
         edge_range=Range(min=0, max=max_int),
-        difficulty=1,
-        # difficulty=0.6,
+        difficulty=0.9,
     ),
-    # Problem(
-    #     label="general",
-    #     vertex_range=Range(min=890, max=900),
-    #     edge_range=Range(min=0, max=max_int),
-    #     difficulty=0.8,
-    # ),
+    Problem(
+        label="general",
+        vertex_range=Range(min=890, max=900),
+        edge_range=Range(min=0, max=max_int),
+        difficulty=1.0,
+    ),
 ]
 
 TIME_LIMITS = [6, 7.5, 10, 15, 30]
